60fee65f482ad27437edd94dea2c69f333f2fc9d
arshiya <158775684+ArshiyaFathi57@users.noreply.github.com>
2024-02-12 10:22:24 +0330

    Update shiralii.c

--- shiralii.c
+++ shiralii.c
@@ -1,14 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int ascii_values[] = {65, 114, 115, 104, 105, 121, 97, 32, 102, 97, 116, 104, 105};
-    
-    printf("ASCII representation of Arshiya fathi:\n");
-    
-    for (int i = 0; i < sizeof(ascii_values) / sizeof(ascii_values[0]); ++i) {
-        printf("%c ", ascii_values[i]);
-    }
-    
-    printf("\n");
+    printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 
+           97, 114, 115, 104, 105, 121, 97, 102, 97, 116, 104, 105, 0, 102, 97, 116, 104, 105);
     return 0;
 }
